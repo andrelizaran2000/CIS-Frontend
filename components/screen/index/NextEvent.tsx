444c6d8a3520bff1f7072abace5f56c5b655e4ec
André Lizarán <105319689+andrelizaran2000@users.noreply.github.com>
2022-09-02 09:40:32 -0500

    feat: Componente que muestra siguiente subevento

--- components/screen/index/NextEvent.tsx
+++ components/screen/index/NextEvent.tsx
@@ -1,39 +1,58 @@
 // Modules
-import React from 'react'
-
-// MUI
-import { blueGrey } from '@mui/material/colors'
-import { Button, Grid, Stack, Typography } from '@mui/material'
-
-// Hooks
-import useTraduction from '../../../hooks/useTraductions'
+import Router from 'next/router';
+import { blueGrey } from '@mui/material/colors';
+import React, { useEffect, useState } from 'react';
+import { Button, Grid, Stack, Typography } from '@mui/material';
 
 // Containers
-import PaddingContainer from '../../containers/PaddingContainer'
+import PaddingContainer from '../../containers/PaddingContainer';
 
-// Images
-import { images } from '../../../utils/sources'
+// Components
+import LoadingScreen from '../../shared/LoadingScreen';
+
+// Hooks
+import useAllRequests from '../../../queries/useAllRequests';
+
+// Types
+import { SubeventRequest } from '../../../types/general';
 
 export default function NextEvent () {
-  const traduce = useTraduction('index')
-  return (
-    <PaddingContainer>
-      <Grid container spacing={4}>
-        <Grid item xs={12} sm={6} sx={{ alignItems:'center', display:'flex' }}>
-          <Stack alignItems='start' rowGap={2}>
-          <Typography variant='h4' color={blueGrey[900]}>
-            <b>{traduce('next-event.title')}</b>
-          </Typography>
-            <Typography variant='h6' sx={{ color:blueGrey[900] }}>{body}</Typography>
-            <Button variant='contained' size='large'>Saber más</Button>
-          </Stack>
-        </Grid>
-        <Grid item xs={12} sm={6} sx={{ display:'flex', alignItems:'end', justifyContent:'end' }}>
-          <img src={images.nextEvent} style={{ width:'100%', maxWidth:450 }}/>
-        </Grid>
-      </Grid>
-    </PaddingContainer>
-  )
-}
 
-const body = 'Conoce mas acerca del camino hacia al exito de Oliver Barrera egresado del Tecnologico de Veracruz'
+  const { useGetEventsQuery } = useAllRequests();
+  const { data, isLoading } = useGetEventsQuery();
+  const [ subeventData, setSubeventData ] = useState<null | SubeventRequest>(null);
+
+  useEffect(() => {
+    if (data !== undefined) {
+      const { data:newData } = data;
+      const { subevents } = newData;
+      if (subevents.length > 0) setSubeventData(subevents[0]);
+    }
+  }, [data]);
+
+  if (isLoading) return <LoadingScreen/>
+  else {
+    return (
+      <PaddingContainer>
+        <Grid container spacing={4}>
+          <Grid item xs={12} sm={6} sx={{ alignItems:'center', display:'flex' }}>
+            <Stack alignItems='start' rowGap={2}>
+            <Typography variant='h4' color={blueGrey[900]}>
+              <b>{subeventData?.name}</b>
+            </Typography>
+              <Typography variant='h6' sx={{ color:blueGrey[900] }}>{subeventData?.description}</Typography>
+              <Button 
+                variant='contained' 
+                size='large' 
+                onClick={() => Router.push(`/subevents/${subeventData?.id}`)}
+              >Saber más</Button>
+            </Stack>
+          </Grid>
+          <Grid item xs={12} sm={6} sx={{ display:'flex', alignItems:'end', justifyContent:'end' }}>
+            <img src={subeventData?.flyer} style={{ width:'100%', maxWidth:450 }}/>
+          </Grid>
+        </Grid>
+      </PaddingContainer>
+    )
+  }
+}
